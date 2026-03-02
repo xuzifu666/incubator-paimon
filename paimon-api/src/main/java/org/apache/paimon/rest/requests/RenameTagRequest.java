9c6ff1db415e9fefe023457fdf4a007e7eade405
xuyu <11161569@vivo.com>
2026-03-02 16:11:17 +0800

    [python] Support rename tag API for RestCatalog

---
+++ paimon-api/src/main/java/org/apache/paimon/rest/requests/RenameTagRequest.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.paimon.rest.requests;
+
+import org.apache.paimon.rest.RESTRequest;
+
+import org.apache.paimon.shade.jackson2.com.fasterxml.jackson.annotation.JsonCreator;
+import org.apache.paimon.shade.jackson2.com.fasterxml.jackson.annotation.JsonGetter;
+import org.apache.paimon.shade.jackson2.com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.apache.paimon.shade.jackson2.com.fasterxml.jackson.annotation.JsonProperty;
+
+/** Request for renaming tag. */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class RenameTagRequest implements RESTRequest {
+
+    private static final String FIELD_OLD_TAG_NAME = "oldTagName";
+    private static final String FIELD_NEW_TAG_NAME = "newTagName";
+
+    @JsonProperty(FIELD_OLD_TAG_NAME)
+    private final String oldTagName;
+
+    @JsonProperty(FIELD_NEW_TAG_NAME)
+    private final String newTagName;
+
+    @JsonCreator
+    public RenameTagRequest(
+            @JsonProperty(FIELD_OLD_TAG_NAME) String oldTagName,
+            @JsonProperty(FIELD_NEW_TAG_NAME) String newTagName) {
+        this.oldTagName = oldTagName;
+        this.newTagName = newTagName;
+    }
+
+    @JsonGetter(FIELD_OLD_TAG_NAME)
+    public String oldTagName() {
+        return oldTagName;
+    }
+
+    @JsonGetter(FIELD_NEW_TAG_NAME)
+    public String newTagName() {
+        return newTagName;
+    }
+}
